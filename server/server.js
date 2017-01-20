044a0c9d381b658f5178af28d481b2f05972bd77
Andrey Fomin <afomin@gmail.com>
2017-01-19 23:30:35 -0500

    Initial setup of Express

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/api/getdata',function(req, res) {
+    res.json([
+        {'id': 1, 'name': 'qwe1'},
+        {'id': 2, 'name': 'qwe2'},
+        {'id': 3, 'name': 'qwe3'}
+    ]);
+});
+
+app.listen(port, function(err){
+    if (!err) {
+        // open('http://localhost:' + port);
+        console.log('Express is running...');
+    } else {
+        console.log(err);
+    }
+});
