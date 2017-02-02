87923653bc7d667f33857c33e75d46df51976508
vachovia <vlad_petrosian@hotmail.com>
2017-02-02 18:47:06 -0500

    minor fixes

--- src/app/recruiter/email.validator.ts
+++ src/app/recruiter/email.validator.ts
@@ -2,15 +2,16 @@ import { AbstractControl } from '@angular/forms';
 
 export const emailValidator = (control:AbstractControl): {[key: string]: boolean} => {
 
-    const recruiterEmail = control.get('recruiterEmail');
-    console.log(recruiterEmail.value);
-    const candidateEmail = control.get('candidateEmail');
-    console.log(candidateEmail.value);
+    // const recruiterEmail = control.get('recruiterEmail');
+    // const candidateEmail = control.get('candidateEmail');
+    // console.log(recruiterEmail.hasError('pattern'));
+    // const emailRegExp = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-    var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-
-    // if (control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
-    //     return { "incorrectMailFormat": true };
+    // if (recruiterEmail!=null && recruiterEmail.value != "" && emailRegExp.test(recruiterEmail.value)) {
+    //     return { "incorrectRecruiterEmailFormat": true };
+    // }
+    // if (candidateEmail!=null && candidateEmail.value != "" && emailRegExp.test(candidateEmail.value)) {
+    //     return { "incorrectCandidateEmailFormat": true };
     // }
 
     return null;
--- src/app/recruiter/recruiter.component.ts
+++ src/app/recruiter/recruiter.component.ts
@@ -16,15 +16,17 @@ export class RecruiterComponent implements OnInit {
     candidateEmail:FormControl;
     base64Emails: string = null;
 
+    emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     constructor(private fb:FormBuilder, private recruiterService: RecruiterService, private router:Router) { }
 
-    ngOnInit() { 
-        this.recruiterEmail = new FormControl(null, [Validators.required]);
-        this.candidateEmail = new FormControl(null, [Validators.compose([Validators.required])]);
+    ngOnInit() {         
+        this.recruiterEmail = new FormControl(null, [Validators.required, Validators.pattern(this.emailRegExp)]);
+        this.candidateEmail = new FormControl(null, [Validators.compose([Validators.required, Validators.pattern(this.emailRegExp)])]);
         this.inviteForm = this.fb.group({
-            'recruiterEmail':this.recruiterEmail,
+            'recruiterEmail': this.recruiterEmail,
             'candidateEmail': this.candidateEmail
-        }, { validator: emailValidator });
+        });//, { validator: emailValidator }
     }
 
     onInvite(inviteFormValue: any):void {
@@ -42,4 +44,11 @@ export class RecruiterComponent implements OnInit {
             );
         //this.router.navigateByUrl('/home');
     } 
+
+    onValidate(control: FormControl){
+        if(control.touched){
+            console.log(control.hasError('pattern'));
+        }
+        return true;
+    }
 }
